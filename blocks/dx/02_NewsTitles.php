715317845dcd720cce448fd60871e0d2fde821a3
John R. D'Orazio <donjohn.fmmi@gmail.com>
2011-03-05 15:43:38 -0800

    cleanup

--- blocks/dx/02_NewsTitles.php
+++ blocks/dx/02_NewsTitles.php
@@ -1,23 +1,5 @@
 <?php
 
-/* Blocco Flatnux che visualizza le notizie della sezione "News"
-   con uno scorrimento verticale automatico.
-   Ogni titolo è un link al messaggio completo.
-   Quando non si è nella sezione news, compare un link alla sezione News in fondo al blocco (in italiano).
-   Utilizza il tag Marquee, e dalla versione 1.5 utilizza la libreria jquery
-   v 1.4 Aggiunta una barra laterale destra che con il mouseover cambia la direzione dello scorrimento in su o in giù.
-   v 1.5 Migliorata la grafica e migliorata la funzionalità della barra laterale destra che controlla con il mouseover sia
-         la direzione che la velocità dello scorrimento, utilizzando la libreria jquery
-   v 1.6 Utilizza jquery per verificare se il blocco si trova effettivamente nella colonna destra oppure no,
-         e lo costruisce dinamicamente a seconda se sta nella colonna destra o sinistra, invertendo tra di loro
-         il marquee e la barra laterale
-   v 1.6.5  Qualche ritocco al aspetto grafico; semplificata la struttura; aggiunto un div sopra che tiene la notizia
-         più recente
-   v 1.7 Aggiunto in fondo il link per aggiungere una notizia se si è amministratori delle news
-   v 2.0 Aggiornato in base alle esigenze di php 5.3 (funzioni ereg* deprecated) e migliorato
-         il modo dell'integrazione con flatnux
-*/
-
 require_once("NewsTitles/config.php");
 
 global $_FN;
