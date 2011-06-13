c3ffa088d00584a53becab2ec3dc8736f44b0278
Lwangaman <donjohn.fmmi@gmail.com>
2011-06-14 01:10:40 +0200

    fixed some encoding problems

--- include/ajax/getnewstitles.php
+++ include/ajax/getnewstitles.php
@@ -1,21 +1,25 @@
 <?php
 /* Blocco Flatnux che visualizza le notizie della sezione "News"
    con uno scorrimento verticale automatico.
-   Ogni titolo è un link al messaggio completo.
-   Quando non si è nella sezione news, compare un link alla sezione News in fondo al blocco (in italiano).
+   Ogni titolo Ã¨ un link al messaggio completo.
+   Quando non si Ã¨ nella sezione news, compare un link alla sezione News in fondo al blocco (in italiano).
    Utilizza il tag Marquee. Dalla versione 1.5 utilizza jquery.
-   v 1.4 Aggiunta una barra laterale destra che con il mouseover cambia la direzione dello scorrimento in su o in giù.
-   v 1.5 Migliorata la grafica e migliorata la funzionalità della barra laterale con l'utilizzo di jquery. Controlla sia
-         la direzione che la velocità dello scorrimento.
+   v 1.4 Aggiunta una barra laterale destra che con il mouseover cambia la direzione dello scorrimento in su o in giÃ¹.
+   v 1.5 Migliorata la grafica e migliorata la funzionalitÃ  della barra laterale con l'utilizzo di jquery. Controlla sia
+         la direzione che la velocitÃ  dello scorrimento.
    v 1.6 Utilizzo di jquery per determinare se il blocco si trova nella colonna destra o sinistra, o lo costruisce
-         dinamicamente affinché la barra laterale sia sempre esterna.
+         dinamicamente affinchÃ© la barra laterale sia sempre esterna.
    v 1.6.5 Ritoccato l'aspetto grafico, corretto un problema col character set che si presenta su alcuni server,
          semplificata la struttura
+   v 1.6.6 Corretto ancora problemi di charset con la funzione get_file() (viene da shared.php, utilizza fgets)
    */
 // *** lwangaman sets the document headers to avoid the Internet Explorer cache-problem
 header( "Cache: no-cache" );
 header( "Pragma: no-cache" );
 // *** end set headers -- lwangaman
+mb_internal_encoding('UTF-8'); // always needed before mb_ functions, check note below
+$opts = array('http' => array('header' => 'Accept-Charset: UTF-8, *;q=0'));
+$context = stream_context_create($opts);
 
 require_once('../flatnux.php');
 
@@ -46,7 +50,7 @@ if ( $perm['group']!=$group_news )
 	else
 	{
 		$fp=fopen("{$_FN['datadir']}/$newsdir/level.php","w");
-		fwrite($fp,"<?php exit(0);?>\n");
+		fwrite($fp,"<?"."php exit(0);?".">\n");
 		fwrite($fp,"\n");
 		fwrite($fp,"\n");
 		fwrite($fp,"$group_news\n");
@@ -90,7 +94,9 @@ $handle = opendir("{$_FN['datadir']}/$newsdir/" . $_FN['lang']);
 		if ( preg_match('/^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].xml$/si',$file) )
 		{
 			$string=get_file("{$_FN['datadir']}/$newsdir/{$_FN['lang']}/$file");
-			$locked=preg_replace("/.*<fn:locked>/si","",$string);
+			if(function_exists("mb_convert_encoding")){ $string = mb_convert_encoding($string, 'HTML-ENTITIES','UTF-8'); }
+      else { $string = htmlentities($string, ENT_QUOTES, "UTF-8"); }
+      $locked=preg_replace("/.*<fn:locked>/si","",$string);
 			$locked=preg_replace("/<\\/fn:locked>.*/si","",$locked);
 			if ( $locked==1 )
 			{
@@ -117,26 +123,29 @@ $handle = opendir("{$_FN['datadir']}/$newsdir/" . $_FN['lang']);
 		if ( $i>$newspp )
 			break;
 		$string=get_file("{$_FN['datadir']}/$newsdir/{$_FN['lang']}/$mod");
+		if(function_exists("mb_convert_encoding")){ $string = mb_convert_encoding($string, 'HTML-ENTITIES','UTF-8'); }
+    else { $string = htmlentities($string, ENT_QUOTES, "UTF-8"); }
 		$title=preg_replace("/.*<fn:title>/s","",$string);
 		$title=preg_replace("/<\\/fn:title>.*/s","",$title);
 		$text=str_replace(".xml","","{$_FN['lang']}/".$mod);
 		$text=str_replace(".php","",$text);
 		global $newstitle;
 		$newstitle[$i] = "<a style='font-weight:bold; color:DarkBlue;' href='index.php?mod=".find_section("news")."&amp;opmod=read&amp;id=".$text."'>".$title."</a><br>";
-                $string = "";
-                if (!isset ($title))
-                 	$title = "";
-                global $jet_lag;
-                $text = str_replace(".xml", "", $text);
-                $newsdata = substr($text, (1 + strpos($text, "/")));
-                $newstitle[$i] .= "<span style='font-size:10;color:DimGray;'>"._POSTATO.date("d-", $newsdata + (3600 * $jet_lag)).date("m-", $newsdata + (3600 * $jet_lag)).date("Y ", $newsdata + (3600 * $jet_lag))."<br> &rArr; ("._LETTO;
-                $string=file_get_contents("{$_FN['datadir']}/$newsdir/$text.xml");
-                $tmp = preg_replace("/.*<fn:reads>/si", "", $string);
-                $tmp = preg_replace("/<\\/fn:reads>.*/s", "", $tmp);
-                $newstitle[$i] .= " $tmp "._VOLTE.")</span>";
-                $newstitle[$i] = iconv("ISO-8859-1","UTF-8",$newstitle[$i]);
-                if (isset($_GET["latestnews"]) && $i==1){echo $newstitle[$i];}
-                elseif (!isset($_GET["latestnews"]) && $i < $newspp){echo "<hr>".$newstitle[$i];}
-                elseif (!isset($_GET["latestnews"])) {echo "<hr>".$newstitle[$i]."<hr>";}
-        }
+    $string = "";
+    if (!isset ($title))
+     	$title = "";
+    global $jet_lag;
+    $text = str_replace(".xml", "", $text);
+    $newsdata = substr($text, (1 + strpos($text, "/")));
+    $newstitle[$i] .= "<span style='font-size:10;color:DimGray;font-family:\"Lucida Sans\";'>"._POSTATO.date('d-', $newsdata + (3600 * $jet_lag)).date('m-', $newsdata + (3600 * $jet_lag)).date('Y ', $newsdata + (3600 * $jet_lag))."<br> â‡’ ("._LETTO;
+    $string=file_get_contents("{$_FN['datadir']}/$newsdir/$text.xml",false,$context);
+    $tmp = preg_replace("/.*<fn:reads>/si", "", $string);
+    $tmp = preg_replace("/<\\/fn:reads>.*/s", "", $tmp);
+    $newstitle[$i] .= " $tmp "._VOLTE.")</span>";
+
+
+    if (isset($_GET["latestnews"]) && $i==1){echo $newstitle[$i];}
+    elseif (!isset($_GET["latestnews"]) && $i < $newspp){echo "<hr>".$newstitle[$i];}
+    elseif (!isset($_GET["latestnews"])) {echo "<hr>".$newstitle[$i]."<hr>";}
+  }
 ?>
